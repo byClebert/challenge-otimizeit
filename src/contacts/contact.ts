0245650245b0f51223122980b7b2371a31191a7a
Clebert Figueiredo <cleberts.figueiredo@outlook.com>
2020-04-27 12:21:57 -0300

    Unnecessary files

--- src/contacts/contact.ts
+++
@@ -1,5 +0,0 @@
-export class Contact {
-    id: number;
-    name: string;
-    email: string;
-}
--- src/contacts/contacts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ContactsController } from './contacts.controller';
-import { ContactService } from './contact.service';
-
-@Module({
-    controllers: [ContactsController],
-    providers: [ContactService]
-})
-export class ContactsModule {}
